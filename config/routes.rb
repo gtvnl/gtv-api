4c03b54802edce0f7dbb4024d08c44c0af86293d
Ramsy de Vos <ramsy.maarten.de.vos@gmail.com>
2016-10-31 21:19:20 +0100

    Add Subdomain

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
-  namespace :api do
+constraints :subdomain => 'api' do
     namespace :v1 do
       resources :items
       resources :sensors
       root to: 'sensors#index'
 
-    end
   end
 
   post 'authenticate', to: 'authentication#authenticate'
   root to: 'home#index'
 
-
+ end
 end
