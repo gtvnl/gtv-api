34e51d385120a7f03f6be012789dec3993a05da2
Ramsy de Vos <r.d.vos@gtv.nl>
2016-11-23 09:09:14 +0100

    .

--- lib/relais.rb
+++ lib/relais.rb
@@ -4,11 +4,13 @@ require 'rpi_gpio'
 class Relais
   class << self
 
-    pins = [12, 16, 18, 22, 24, 26, 32, 36]
+
 
     def setup
       RPi::GPIO.set_warnings(false)
       RPi::GPIO.set_numbering :board
+
+      pins = [12, 16, 18, 22, 24, 26, 32, 36]
 
       pins.each do |pin|
         RPi::GPIO.setup pin, :as => :output, :initialize => :high
@@ -24,12 +26,18 @@ class Relais
     end
 
     def all_on
+
+      pins = [12, 16, 18, 22, 24, 26, 32, 36]
+
       pins.each do |pin|
         RPi::GPIO.set_low pin
       end
     end
 
     def all_off
+
+      pins = [12, 16, 18, 22, 24, 26, 32, 36]
+
       pins.each do |pin|
         RPi::GPIO.set_high number
       end
