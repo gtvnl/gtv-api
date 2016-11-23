8646e0500b08fcbdc9f9901446860288a8f78800
Ramsy de Vos <r.d.vos@gtv.nl>
2016-11-23 16:22:34 +0100

    Sensors

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,18 +27,36 @@ puts "Creating Setpoints ...\n"
   Setpoint.find_or_create_by(name: "Setpoint #{index}") do |setpoint|
     setpoint.name = "Setpoint #{index}"
     setpoint.value = -20.0
+    Log.create(description: "CREATE: Setpoint #{setpoint.name} with value #{setpoint.value} &deg;C", value: setpoint.value)
+
   end
 end
 
 puts "Creating GPIOs ...\n"
-Gpio.create(name: "Relais 1", gpio_number: 18, pin: 12, of_type: 'output')
-Gpio.create(name: "Relais 2", gpio_number: 23, pin: 16, of_type: 'output')
-Gpio.create(name: "Relais 3", gpio_number: 24, pin: 18, of_type: 'output')
-Gpio.create(name: "Relais 4", gpio_number: 25, pin: 22, of_type: 'output')
-Gpio.create(name: "Relais 5", gpio_number: 8, pin: 24, of_type: 'output')
-Gpio.create(name: "Relais 6", gpio_number: 7, pin: 26, of_type: 'output')
-Gpio.create(name: "Relais 7", gpio_number: 12, pin: 32, of_type: 'output')
-Gpio.create(name: "Relais 8", gpio_number: 16, pin: 36, of_type: 'output')
+gpio1 = Gpio.create(name: "Relais 1", gpio_number: 18, pin: 12, of_type: 'output')
+Log.create(description: "CREATE: GPIO #{gpio1.name} (GPIO:#{gpio1.gpio_number}/PIN:#{gpio1.pin}")
+
+gpio2 = Gpio.create(name: "Relais 2", gpio_number: 23, pin: 16, of_type: 'output')
+Log.create(description: "CREATE: GPIO #{gpio2.name} (GPIO:#{gpio2.gpio_number}/PIN:#{gpio2.pin}")
+
+gpio3 = Gpio.create(name: "Relais 3", gpio_number: 24, pin: 18, of_type: 'output')
+Log.create(description: "CREATE: GPIO #{gpio3.name} (GPIO:#{gpio3.gpio_number}/PIN:#{gpio3.pin}")
+
+gpio4 = Gpio.create(name: "Relais 4", gpio_number: 25, pin: 22, of_type: 'output')
+Log.create(description: "CREATE: GPIO #{gpio4.name} (GPIO:#{gpio4.gpio_number}/PIN:#{gpio4.pin}")
+
+gpio5 = Gpio.create(name: "Relais 5", gpio_number: 8, pin: 24, of_type: 'output')
+Log.create(description: "CREATE: GPIO #{gpio5.name} (GPIO:#{gpio5.gpio_number}/PIN:#{gpio5.pin}")
+
+gpio6 = Gpio.create(name: "Relais 6", gpio_number: 7, pin: 26, of_type: 'output')
+Log.create(description: "CREATE: GPIO #{gpio6.name} (GPIO:#{gpio6.gpio_number}/PIN:#{gpio6.pin}")
+
+gpio7 = Gpio.create(name: "Relais 7", gpio_number: 12, pin: 32, of_type: 'output')
+Log.create(description: "CREATE: GPIO #{gpio7.name} (GPIO:#{gpio7.gpio_number}/PIN:#{gpio7.pin}")
+
+gpio8 = Gpio.create(name: "Relais 8", gpio_number: 16, pin: 36, of_type: 'output')
+Log.create(description: "CREATE: GPIO #{gpio8.name} (GPIO:#{gpio8.gpio_number}/PIN:#{gpio8.pin}")
+
 
 # if RbConfig::CONFIG['host_os'] == "linux-gnueabihf"
 #   puts "Scanning for Sensors ...\n"
