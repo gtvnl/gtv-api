1b6d6b190ec48af16740856264642064f1b7112c
Ramsy de Vos <r.d.vos@gtv.nl>
2016-11-23 12:26:17 +0100

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
+puts "Creating Administrator ...\n"
 User.find_or_create_by(email: "r.d.vos@gtv.nl") do |user|
   user.name = "Administrator"
   user.password = "Admin1234!"
   user.password_confirmation = "Admin1234!"
-  Log.create(description: "Added an Administrator named '#{user.name}' with password '#{user.spassword}'")
+  Log.create(description: "Added an Administrator named '#{user.name}' with password '#{user.password}'")
 end
 
+puts "Creating API key ...\n"
 apikey = AuthenticateUser.call("r.d.vos@gtv.nl", "Admin1234!").result
 user = User.find_by(email: "r.d.vos@gtv.nl")
 
@@ -21,6 +22,7 @@ user.apikey = apikey
 user.save
 Log.create(description: "Added an API key for '#{user.name}':'#{apikey}'")
 
+puts "Creating Setpoints ...\n"
 (1..6).each do |index|
   Setpoint.find_or_create_by(name: "Setpoint #{index}") do |setpoint|
     setpoint.name = "Setpoint #{index}"
@@ -28,7 +30,7 @@ Log.create(description: "Added an API key for '#{user.name}':'#{apikey}'")
   end
 end
 
-
+puts "Creating GPIOs ...\n"
 Gpio.create(name: "Relais 1", gpio: 18, pin: 12, of_type: 'output')
 Gpio.create(name: "Relais 2", gpio: 23, pin: 16, of_type: 'output')
 Gpio.create(name: "Relais 3", gpio: 24, pin: 18, of_type: 'output')
@@ -38,5 +40,5 @@ Gpio.create(name: "Relais 6", gpio: 7, pin: 26, of_type: 'output')
 Gpio.create(name: "Relais 7", gpio: 12, pin: 32, of_type: 'output')
 Gpio.create(name: "Relais 8", gpio: 16, pin: 36, of_type: 'output')
 
-
+puts "Scanning for Sensors ...\n"
 Sensors.scan
