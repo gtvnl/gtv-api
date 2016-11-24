20d534a77fa88d2c3bd59a9a6b2ac0743c302f14
Ramsy de Vos <r.d.vos@gtv.nl>
2016-11-24 16:38:55 +0100

    Add Python polling

---
+++ poll.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)     # set up BCM GPIO numbering  
+GPIO.setup(23, GPIO.IN)    # set GPIO25 as input (button)  
+  
+# Define a threaded callback function to run in another thread when events are detected  
+def my_callback(channel):  
+    if GPIO.input(23):     # if port 25 == 1  
+        print "Pulse detected: " + time.ctime()  
+  
+# when a changing edge is detected on port 25, regardless of whatever   
+# else is happening in the program, the function my_callback will be run  
+GPIO.add_event_detect(23, GPIO.FALLING, callback=my_callback)  
+  
+raw_input("Press Enter when ready\n>")  
+  
+try:  
+    print "Waiting for pulse ..."  
+    sleep(300)         # wait 30 seconds  
+    print "Time's up. Finished!"  
+  
+finally:                   # this block will run no matter how the try block exits  
+    GPIO.cleanup()         # clean up after yourself  
