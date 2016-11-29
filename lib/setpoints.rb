703d75111ba9e85a1ef94e9f6180113f0d817eb3
Ramsy de Vos <r.d.vos@gtv.nl>
2016-11-29 13:16:46 +0100

    add setpoints

--- lib/setpoints.rb
+++ lib/setpoints.rb
@@ -13,6 +13,24 @@ class Setpoints
   def check(setpoint)
     if !setpoint.is_a? Setpoint
       puts "Invalid input. Use a setpoint"
+    else
+      if setpoint.sensor.nil?
+        puts "Configure the target sensor first."
+      elsif setpoint.gpio.nil?
+        puts "Configure the target GPIO first."
+      else
+        # Assume we have valid data
+        current_temp = Sensors.check_one(setpoint.sensor)
+        min_temp = setpoint.value
+
+        if current_temp < min_temp
+          puts "Temperatuur is te laag."
+        elsif current_temp > min_temp
+          puts "Temperatuur is te hoog."
+        end
+
+
+      end
     end
   end
 
