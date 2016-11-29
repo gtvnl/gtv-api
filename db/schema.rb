81d27eb0ab3bdee0f8d45fde0192557558d3c4a7
Ramsy de Vos <r.d.vos@gtv.nl>
2016-11-29 08:19:53 +0100

    Add pin to setpoint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161124120542) do
+ActiveRecord::Schema.define(version: 20161129071834) do
 
   create_table "gpios", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20161124120542) do
     t.float    "value",      limit: 24
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
+    t.integer  "pin"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
