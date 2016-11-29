884e996f3987853fc945583ffb1d3445f6ee396f
Ramsy de Vos <r.d.vos@gtv.nl>
2016-11-29 16:12:03 +0100

    .

--- app/controllers/chart_controller.rb
+++ app/controllers/chart_controller.rb
@@ -1,5 +1,24 @@
 class ChartController < ApplicationController
   def index
-    render json: "Chart"
+    hash = Hash.new
+
+    @sensors = Sensor.all
+
+    @sensors.each do |sensor|
+      @logs = Log.where(sensor: sensor.name)
+
+      @logs.each do |log|
+        newHash = {
+          sensor: log.sensor
+          value: log.value
+          date: log.created_at
+        }
+        hash.merge!(newHash)
+      end
+
+
+    end
+
+    render json: hash
   end
 end
