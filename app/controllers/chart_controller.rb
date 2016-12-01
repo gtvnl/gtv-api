645d16fc864a15cc13b2e990e2057bc666537bd2
Ramsy de Vos <ramsy.thailand@gmail.com>
2016-12-01 20:08:23 +0100

    .

--- app/controllers/chart_controller.rb
+++ app/controllers/chart_controller.rb
@@ -1,42 +1,14 @@
 class ChartController < ApplicationController
   def index
-    @sensor1 = Sensor.find_by(id: 1)
-    @sensor2 = Sensor.find_by(id: 2)
-    @sensor3 = Sensor.find_by(id: 3)
-    @sensor4 = Sensor.find_by(id: 4)
-    @sensor5 = Sensor.find_by(id: 5)
-    @sensor6 = Sensor.find_by(id: 6)
 
-    @sa1 = ["Sensor 1"];
-    @sa2 = ["Sensor 2"];
-    @sa3 = ["Sensor 3"];
-    @sa4 = ["Sensor 4"];
-    @sa5 = ["Sensor 5"];
-    @sa6 = ["Sensor 6"];
+    values = []
+    @logs = Log.where(sensor: "Sensor 2")
 
-    Log.where(sensor: @sensor1) do |sensor|
-      @sa1 << sensor.value
-    end
-    Log.where(sensor: @sensor2) do |sensor|
-      @sa2 << sensor.value
-    end
-    Log.where(sensor: @sensor3) do |sensor|
-      @sa3 << sensor.value
-    end
-    Log.where(sensor: @sensor4) do |sensor|
-      @sa4 << sensor.value
-    end
-    Log.where(sensor: @sensor5) do |sensor|
-      @sa5 << sensor.value
-    end
-    Log.where(sensor: @sensor6) do |sensor|
-      @sa6 << sensor.value
+    @logs.each do |log|
+      values << log.value
     end
 
-
-    @total = [@sa1, @sa2, @sa3, @sa4, @sa5, @sa6]
-
-    render json: @sa1, meta: default_meta
+    render json: @values, meta: default_meta
 
   end
 end
