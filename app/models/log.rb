69c0ea28ce7791692bc7ed9120a580d165404e89
Ramsy de Vos <ramsy.thailand@gmail.com>
2016-12-05 18:13:03 +0100

    .

--- app/models/log.rb
+++ app/models/log.rb
@@ -7,7 +7,7 @@ class Log < ApplicationRecord
 def make_hash
   h = Hash.new
   Log.where(sensor: "2b").pluck_h(:created_at, :value).each do |item|
-    puts { item.created_at => item.value }
+    # puts { item.created_at => item.value }
   end
 
   return h
