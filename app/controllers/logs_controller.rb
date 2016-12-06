2031d8ceb6309248e5c0c2f3c72136553966365f
Ramsy de Vos <r.d.vos@gtv.nl>
2016-12-06 08:37:06 +0100

    .

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -3,13 +3,13 @@ class LogsController < ApplicationController
   before_action :set_log, only: [:show, :update, :destroy]
 
   def sensor_2a
-    result = Log.where(sensor: "2a").where(updated_at: (Time.now - 24.hours)..Time.now).inject({}){|h,e| h.merge(e.created_at => e.value) }
-    render json: [{name: 'Sensor 2a', data: result}]
+    render json: Log.where(sensor: "2a").where(updated_at: (Time.now - 24.hours)..Time.now).inject({}){|h,e| h.merge(e.created_at => e.value) }
+    # render json: [{name: 'Sensor 2a', data: result}]
   end
 
   def sensor_2b
-    result = Log.where(sensor: "2b").where(updated_at: (Time.now - 24.hours)..Time.now).inject({}){|h,e| h.merge(e.created_at => e.value) }
-    render json: [{name: 'Sensor 2b', data: result}]
+    render json: Log.where(sensor: "2b").where(updated_at: (Time.now - 24.hours)..Time.now).inject({}){|h,e| h.merge(e.created_at => e.value) }
+    # render json: [{name: 'Sensor 2b', data: result}]
   end
 
   # GET /logs
