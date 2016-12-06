c468ecf9b52a329159e17d4aa0ddb79def72f1e8
Ramsy de Vos <r.d.vos@gtv.nl>
2016-12-06 09:19:26 +0100

    .

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -35,27 +35,45 @@ class ChartsController < ApplicationController
     @sensor_6b = Log.where(sensor: "6b").where(updated_at: (Time.now - 24.hours)..Time.now).inject({}){|h,e| h.merge(e.created_at => e.value) }
     @sensor_6c = Log.where(sensor: "6c").where(updated_at: (Time.now - 24.hours)..Time.now).inject({}){|h,e| h.merge(e.created_at => e.value) }
 
-    @data = [
-  {name: "Sensor 1a", data: @sensor_1a},
-  {name: "Sensor 1b", data: @sensor_1b},
-  {name: "Sensor 1c", data: @sensor_1c},
-  {name: "Sensor 2a", data: @sensor_2a},
-  {name: "Sensor 2b", data: @sensor_2b},
-  {name: "Sensor 2c", data: @sensor_2c},
-  {name: "Sensor 3a", data: @sensor_3a},
-  {name: "Sensor 3b", data: @sensor_3b},
-  {name: "Sensor 3c", data: @sensor_3c},
-  {name: "Sensor 4a", data: @sensor_4a},
-  {name: "Sensor 4b", data: @sensor_4b},
-  {name: "Sensor 4c", data: @sensor_4c},
-  {name: "Sensor 5a", data: @sensor_5a},
-  {name: "Sensor 5b", data: @sensor_5b},
-  {name: "Sensor 5c", data: @sensor_5c},
-  {name: "Sensor 6a", data: @sensor_6a},
-  {name: "Sensor 6b", data: @sensor_6b},
-  {name: "Sensor 6c", data: @sensor_6c}
+    @setpoint1 = Setpoint.find_by(name: "Setpoint 1").value
+    @setpoint2 = Setpoint.find_by(name: "Setpoint 2").value
+    @setpoint3 = Setpoint.find_by(name: "Setpoint 3").value
+    @setpoint4 = Setpoint.find_by(name: "Setpoint 4").value
+    @setpoint5 = Setpoint.find_by(name: "Setpoint 5").value
+    @setpoint6 = Setpoint.find_by(name: "Setpoint 6").value
 
-]
+    @data = [
+      {name: "Setpoint 1", data: @setpoint1},
+      {name: "Sensor 1a", data: @sensor_1a},
+      {name: "Sensor 1b", data: @sensor_1b},
+      {name: "Sensor 1c", data: @sensor_1c},
+
+      {name: "Setpoint 2", data: @setpoint2},
+      {name: "Sensor 2a", data: @sensor_2a},
+      {name: "Sensor 2b", data: @sensor_2b},
+      {name: "Sensor 2c", data: @sensor_2c},
+
+      {name: "Setpoint 3", data: @setpoint3},
+      {name: "Sensor 3a", data: @sensor_3a},
+      {name: "Sensor 3b", data: @sensor_3b},
+      {name: "Sensor 3c", data: @sensor_3c},
+
+      {name: "Setpoint 4", data: @setpoint4},
+      {name: "Sensor 4a", data: @sensor_4a},
+      {name: "Sensor 4b", data: @sensor_4b},
+      {name: "Sensor 4c", data: @sensor_4c},
+
+      {name: "Setpoint 5", data: @setpoint5},
+      {name: "Sensor 5a", data: @sensor_5a},
+      {name: "Sensor 5b", data: @sensor_5b},
+      {name: "Sensor 5c", data: @sensor_5c},
+
+      {name: "Setpoint 6", data: @setpoint6},
+      {name: "Sensor 6a", data: @sensor_6a},
+      {name: "Sensor 6b", data: @sensor_6b},
+      {name: "Sensor 6c", data: @sensor_6c}
+    ]
+
     render "charts/index"
 
   end
