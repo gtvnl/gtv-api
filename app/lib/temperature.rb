c5387eafc6b2995e65becd337d3befb2d2c78e41
Ramsy de Vos <ramsy.thailand@gmail.com>
2016-12-08 18:33:48 +0100

    .

--- app/lib/temperature.rb
+++ app/lib/temperature.rb
@@ -2,53 +2,51 @@ require "serialport"
 class Temp
   class << self
 
-def read
+    def read
 
-port_str = "/dev/ttyACM0"  #may be different for you
-baud_rate = 115200
-data_bits = 8
-stop_bits = 1
-parity = SerialPort::NONE
+    port_str = "/dev/ttyACM0"  #may be different for you
+    baud_rate = 115200
+    data_bits = 8
+    stop_bits = 1
+    parity = SerialPort::NONE
+    h = Hash.new
+    sp = SerialPort.open(port_str, baud_rate, data_bits, stop_bits, parity)
 
-sp = SerialPort.open(port_str, baud_rate, data_bits, stop_bits, parity)
+    sensorNames = ["1a","1b","1c","2a","2b","2c","3a","3b","3c","4a","4b","4c","5a","5b","5c","6a","6b","6c"]
 
-sensorNames = ["1a","1b","1c","2a","2b","2c","3a","3b","3c","4a","4b","4c","5a","5b","5c","6a","6b","6c"]
+    eofCount = 0
+    serialString = ""
 
-eofCount = 0
+    while eofCount < 5
 
-serialString = ""
+      i = sp.gets.chomp
 
-while eofCount < 5
+      unless i.nil?
+        serialString << i
+        if i.include? "EOF"
+          eofCount += 1
 
-  i = sp.gets.chomp
+        end
+      end
+    end
 
-  unless i.nil?
-    serialString << i
-    if i.include? "EOF"
-      eofCount += 1
+    sp.close
+
+    #result = /START>{3}(.*?)\<{3}EOF/.match(serialString)
+
+    array = serialString.split(",")
+
+      array.each do |value|
+
+        name = sensor.split(":")[0]
+        temp = sensor.split(":")[1].to_f
+
+        h = {"#{s}":  t}
+
+        puts h
+
+      end
 
     end
   end
- puts serialString
-end
-
-
-sp.close
-
-#result = /START>{3}(.*?)\<{3}EOF/.match(serialString)
-
-array = serialString.split(",")
-
-array.each do |sensor|
-
- s = sensor.split(":")[0]
- t = s[1]
-
- h = {"#{s}":  t}
- 
-puts h
-end
-
-end	
-end
 end
