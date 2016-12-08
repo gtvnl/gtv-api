7de363b3d07656381ebf60780c84f84b1d0252d5
Ramsy de Vos <r.d.vos@gtv.nl>
2016-12-08 12:27:49 +0100

    .

--- temperature.rb
+++ temperature.rb
@@ -9,18 +9,18 @@ parity = SerialPort::NONE
 
 sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
 
-sensors = 20
+sensors = 12
 count = 0
 
-hash = Hash.new
+set = Set.new
 begin
   i = sp.gets.chomp unless sp.gets.nil?
   if i.include? "Sensor"
 
     name = i.split(": ")[0]
     temp = i.split(": ")[1].to_f
-    hash[name] = temp
-    puts "[#{hash.count}]#{name}: #{temp}"
+    set << {"#{name}": temp}
+    puts "[#{set.count}]#{name}: #{temp}"
   end
 end while hash.count < sensors
 
