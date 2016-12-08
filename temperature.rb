d72c742a3a0cab21911475a4a3f3c974724ab72b
Ramsy de Vos <r.d.vos@gtv.nl>
2016-12-08 13:51:55 +0100

    .

--- temperature.rb
+++ temperature.rb
@@ -11,13 +11,26 @@ sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
 
 sensorNames = ["1a","1b","1c","2a","2b","2c","3a","3b","3c","4a","4b","4c","5a","5b","5c","6a","6b","6c","Binnen","Buiten"]
 
+startCount = 0
+endCount = 0
+serialString = ""
 
-begin
-  i = sp.gets.chomp
+while (startCount == 2 && endCount == 2)
 
-  puts i
+  i = sp.gets
 
-end while i.nl?
+  unless i.nil?
+    if i.include? "START>>>"
+      startCount += 1
+      serialString = serialString + i
+    elsif i.incude? "<<<EOF"
+      endCount += 1
+      serialString = serialString + i
+    end
+  end
 
+end
+
+puts serialString
 
 sp.close
