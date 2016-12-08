e67f84e633e5984fd07cb1ce2248305bad039bee
Ramsy de Vos <r.d.vos@gtv.nl>
2016-12-08 12:15:25 +0100

    .

---
+++ temperature.rb
@@ -0,0 +1,27 @@
+require "serialport"
+
+port_str = "/dev/ttyACM0"  #may be different for you
+baud_rate = 9600
+data_bits = 8
+stop_bits = 1
+parity = SerialPort::NONE
+
+sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
+sp.close
+sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
+
+
+sensors = 20
+count = 0
+
+hash = Hash.new
+   begin  begin
+      i = sp.gets.chomp unless sp.gets.nil?
+      if i.include? "Sensor"
+        name = i.split(": ")[0]
+        temp = i.split(": ")[1].to_f
+        hash[name] = temp
+      end
+   end while count < sensors
+puts hash
+sp.close
