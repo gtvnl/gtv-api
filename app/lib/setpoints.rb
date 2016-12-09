f13f66aedf1e4c90b83ab84c2e58fd4db7831654
Ramsy de Vos <r.d.vos@gtv.nl>
2016-12-09 08:21:22 +0100

    .

--- app/lib/setpoints.rb
+++ app/lib/setpoints.rb
@@ -33,7 +33,7 @@ class Setpoints
           # Assume we have valid data
           relais = setpoint.gpio.pin
 
-          current_temp = Temp.read["#{setpoint.sensor[:name]}"]
+          current_temp = Serial.read["#{setpoint.sensor[:name]}"]
           desired_temp = setpoint.value
           min_temp = setpoint.value - setpoint.max_temp_difference
           max_temp = setpoint.value + setpoint.max_temp_difference
