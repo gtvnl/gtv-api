edf787b6621de344d301aa0f3d78a3555a0f770f
Ramsy de Vos <r.d.vos@gtv.nl>
2017-01-16 15:31:38 +0100

    .

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,13 +6,13 @@ every 1.day, :at => '12:00 am' do
 end
 
 every 5.minutes do
-  command "/usr/bin/python /home/pi/gtv-api/log.py"
   runner "UpdateSensorsJob.perform_now"
   runner "CheckSetpointsJob.perform_now"
   runner "CheckPowerSupplyJob.perform_now"
 end
 
 every 1.minute do
+  command "/usr/bin/python /home/pi/gtv-api/log.py"
   command "/home/pi/gtv-api/create_webserver_tunnel.sh 2>&1"
   command "/home/pi/gtv-api/create_ssh_tunnel.sh 2>&1"
 end
