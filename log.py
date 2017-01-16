796ea615296c358ac26dd562e47d17678f0867b6
Ramsy de Vos <r.d.vos@gtv.nl>
2017-01-16 15:47:00 +0100

    .

--- log.py
+++ log.py
@@ -16,8 +16,9 @@ with ser as pt, open(fname,fmode) as outf:
    spb = io.TextIOWrapper(io.BufferedRWPair(pt,pt,1),
      encoding='ascii', errors='ignore', newline='\r',line_buffering=True)
    spb.readline()  # throw away first line; likely to start mid-sentence (incomplete)
-while (1):
+while ser.isOpen():
     x = spb.readline() # read one line of text from serial port
     print (x,end='')   #echo line of text on-screen
     outf.write(x)      #write line of text to file
     outf.flush()       #make sure it actually gets written out
+    ser.close()
