2ee48c264770b682c083f2a30f6a79bbb36616f4
Ramsy de Vos <r.d.vos@gtv.nl>
2017-01-16 15:44:50 +0100

    .

--- log.py
+++ log.py
@@ -2,26 +2,22 @@
 from datetime import datetime
 import serial, io
 
-outfile='/home/pi/gtv-api/temp.dat'
+fname='/home/pi/gtv-api/temp.dat'
+fmode='w'
 
 ser = serial.Serial(
     port='/dev/ttyACM0',
     baudrate=9600,
 )
 
-sio = io.TextIOWrapper(
-    io.BufferedRWPair(ser, ser, 1),
-    encoding='ascii', newline='\r'
-)
 
-with open(outfile,'w') as f: #appends to existing file
-    while ser.isOpen():
-      sio.readline()
-      ser.flushInput() #flush input buffer, discarding all its contents
-      ser.flushOutput()
-      datastring = sio.readline()
-      print(datastring)
-      #\t is tab; \n is line separator
-      f.write(datetime.utcnow().isoformat() + '\t' + datastring + '\n')
-      f.flush() #included to force the system to write to disk
-      ser.close()
+
+with ser as pt, open(fname,fmode as outf:
+   spb = io.TextIOWrapper(io.BufferedRWPair(pt,pt,1),
+     encoding='ascii', errors='ignore', newline='\r',line_buffering=True)
+   spb.readline()  # throw away first line; likely to start mid-sentence (incomplete)
+ while (1):
+     x = spb.readline() # read one line of text from serial port
+     print (x,end='')   #echo line of text on-screen
+     outf.write(x)      #write line of text to file
+     outf.flush()       #make sure it actually gets written out
