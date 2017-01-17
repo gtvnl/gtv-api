fa6aac1eec5e3b662ec03530ccfafff4deec4368
Ramsy de Vos <r.d.vos@gtv.nl>
2017-01-17 17:00:18 +0100

    .

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,10 @@ Rails.application.routes.draw do
   resources :logs
 
   get 'charts', to: 'charts#index'
+  get '12hr', to: 'charts#12hr'
+  get '6hr', to: 'charts#6hr'
+  get '3hr', to: 'charts#3hr'
+  get '1hr', to: 'charts#1hr'
 
   get 'sensor_2a', to: 'logs#sensor_2a'
   get 'sensor_2b', to: 'logs#sensor_2b'
