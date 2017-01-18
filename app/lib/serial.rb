b4c627e47490d219001eb1fb78d5ff53ebd90cfe
Ramsy de Vos <r.d.vos@gtv.nl>
2017-01-18 13:17:30 +0100

    .

--- app/lib/serial.rb
+++ app/lib/serial.rb
@@ -12,7 +12,7 @@ class Serial
 
     serialString = File.open('temp.dat', 'r') { |file| file.read }
 
-    unless serialString.blank?
+    unless serialString.blank? && !last_read
       h = Hash.new
 
       sensorNames = ["1a","1b","1c","2a","2b","2c","3a","3b","3c","4a","4b","4c","5a","5b","5c","6a","6b","6c","Binnen","Buiten"]
