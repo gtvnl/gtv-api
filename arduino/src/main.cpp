022a425a138beed8bd91b2b51c59897406a5a5eb
Ramsy de Vos <r.d.vos@gtv.nl>
2017-01-18 10:45:31 +0100

    .

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -64,31 +64,58 @@ void setup(void)
 {
  Serial.begin(9600);
  sensor1a.begin();
+ sensor1a.setResolution(10);
  sensor1b.begin();
+ sensor1b.setResolution(10);
  sensor1c.begin();
+ sensor1c.setResolution(10);
 
  sensor2a.begin();
+ sensor2a.setResolution(10);
  sensor2b.begin();
+ sensor2b.setResolution(10);
  sensor2c.begin();
+ sensor2c.setResolution(10);
+
 
  sensor3a.begin();
+ sensor3a.setResolution(10);
  sensor3b.begin();
+ sensor3b.setResolution(10);
  sensor3c.begin();
+ sensor3c.setResolution(10);
+
 
  sensor4a.begin();
+ sensor4a.setResolution(10);
  sensor4b.begin();
+ sensor4b.setResolution(10);
  sensor4c.begin();
+ sensor4c.setResolution(10);
+
 
  sensor5a.begin();
+ sensor5a.setResolution(10);
  sensor5b.begin();
+ sensor5b.setResolution(10);
  sensor5c.begin();
+ sensor5c.setResolution(10);
+
 
  sensor6a.begin();
+ sensor6a.setResolution(10);
  sensor6b.begin();
+ sensor6b.setResolution(10);
  sensor6c.begin();
+ sensor6c.setResolution(10);
+
 
  sensorBinnen.begin();
+ sensorBinnen.setResolution(10);
+
  sensorBuiten.begin();
+ sensorBuiten.setResolution(10);
+
 
 }
 
