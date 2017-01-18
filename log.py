e53ea962e6e34dad13b5e2b5510ac2be1b819175
Ramsy de Vos <r.d.vos@gtv.nl>
2017-01-18 08:32:38 +0100

    .

--- log.py
+++ log.py
@@ -11,15 +11,17 @@ ser = serial.Serial(
 
 sio = io.TextIOWrapper(
     io.BufferedRWPair(ser, ser, 1),
-    encoding='ascii', newline='\r', line_buffering=True
+    encoding='ascii', newline='\r'
 )
 
 with open(outfile,'w') as f: #appends to existing file
     while ser.isOpen():
       sio.readline()
       datastring = sio.readline()
-      print(datastring)
-      #\t is tab; \n is line separator
-      f.write(datetime.utcnow().isoformat() + '\t' + datastring + '\n')
-      f.flush() #included to force the system to write to disk
+      length = len(datastring.split(','))
+      if length == 20:
+          print(datastring)
+          f.write(datetime.utcnow().isoformat() + '\t' + datastring + '\n')
+          f.flush()
+
       ser.close()
