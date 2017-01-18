96207cbb8acde8491b6b30b62a9b4eea9a15cb72
Ramsy de Vos <r.d.vos@gtv.nl>
2017-01-18 08:53:13 +0100

    .

--- app/lib/serial.rb
+++ app/lib/serial.rb
@@ -5,34 +5,39 @@ class Serial
 
     serialString = File.open('temp.dat', 'r') { |file| file.read }
 
+    unless serialString.blank?
+      h = Hash.new
 
-    h = Hash.new
+      sensorNames = ["1a","1b","1c","2a","2b","2c","3a","3b","3c","4a","4b","4c","5a","5b","5c","6a","6b","6c","Binnen","Buiten"]
 
-    sensorNames = ["1a","1b","1c","2a","2b","2c","3a","3b","3c","4a","4b","4c","5a","5b","5c","6a","6b","6c","Binnen","Buiten"]
+      array = serialString.split("\t")[1].split("\r")[0].split(",")
 
-    array = serialString.split("\t")[1].split("\r")[0].split(",")
+        array.each do |value|
 
-      array.each do |value|
+          name = value.split(":")[0]
+          temp = value.split(":")[1].to_f
 
-        name = value.split(":")[0]
-        temp = value.split(":")[1].to_f
+          unless name.include? "EOF"
+            h["#{name}"] = temp
+          end
 
-        unless name.include? "EOF"
-          h["#{name}"] = temp
         end
 
-      end
+        if h.count == 20
+          puts serialString
+          FileUtils.rm('temp.dat')
 
-      if h.count == 20
-        puts serialString
-        return h.sort.to_h
-        FileUtils.rm('temp.dat')
+          return h.sort.to_h
 
+
+
+        else
+          puts "Incomplete data"
+        end
 
       else
-        puts "Incomplete data"
+        puts "temp.dat is empty"
       end
-
     end
   end
 end
