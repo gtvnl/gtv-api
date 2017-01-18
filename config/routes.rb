e1bbb5b5382654a205d9c6562bf7bfd629186f59
Ramsy de Vos <r.d.vos@gtv.nl>
2017-01-18 14:31:55 +0100

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
 
-
+  # config/routes.rb
+  require "sidekiq/web"
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    # Protect against timing attacks:
+    # - See https://codahale.com/a-lesson-in-timing-attacks/
+    # - See https://thisdata.com/blog/timing-attacks-against-string-comparison/
+    # - Use & (do not use &&) so that it doesn't short circuit.
+    # - Use digests to stop length information leaking
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest("admin")) &
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest("Raspberry1!"))
+  end if Rails.env.production?
+  mount Sidekiq::Web, at: "/sidekiq"
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
