7fa2ccbe631e31b5b558027a164fa4a0237f602b
Ramsy M. de Vos <ramsy.maarten.de.vos@gmail.com>
2018-01-30 15:33:58 +0100

    .

---
+++ app/lib/rails_admin_logout_link.rb
@@ -0,0 +1,16 @@
+# lib/rails_admin_logout_link.rb
+RailsAdmin::ApplicationHelper
+
+  module RailsAdmin
+    module ApplicationHelper
+      def logout_path
+        main_app.send(:sign_out_path) rescue false
+      end
+    end
+  end
+
+  class Devise
+    def self.sign_out_via
+      :delete
+    end
+  end
