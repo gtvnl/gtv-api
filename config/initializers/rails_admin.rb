59067ab0b5b2d4be84b9f551448de4f7a3ef15ff
Ramsy M. de Vos <ramsy.maarten.de.vos@gmail.com>
2018-01-30 15:40:33 +0100

    .

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -9,7 +9,7 @@ RailsAdmin.config do |config|
   # config.authenticate_with do
   #   warden.authenticate! scope: :user
   # end
-  # config.current_user_method(&:current_user)
+  config.current_user_method(&:current_user)
 
   ## == Cancan ==
   # config.authorize_with :cancan
@@ -18,13 +18,13 @@ RailsAdmin.config do |config|
   # config.authorize_with :pundit
 
   ## == PaperTrail ==
-  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
+  config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
 
   ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
 
   ## == Gravatar integration ==
   ## To disable Gravatar integration in Navigation Bar set to false
-  # config.show_gravatar true
+  config.show_gravatar true
 
   config.actions do
     dashboard                     # mandatory
@@ -57,7 +57,7 @@ RailsAdmin.config do |config|
 
 
     ## With an audit adapter, you can add:
-    # history_index
-    # history_show
+    history_index
+    history_show
   end
 end
