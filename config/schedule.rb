413e8f9f707346854766d6c8b6fe90bd466540fd
Ramsy M. de Vos <ramsy.maarten.de.vos@gmail.com>
2018-01-31 13:24:46 +0100

    .

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 # Learn more: http://github.com/javan/whenever
-job_type :runner,  "cd :path && /home/pi/.rbenv/shims/bundle exec bin/rails runner -e :environment ':task' :output"
+job_type :runner,  "cd :path && :environment_variable=:environment /home/pi/.rbenv/shims/bundle exec bin/rails runner -e :environment ':task' :output"
 
 every 1.day, :at => '12:00 am' do
   runner "DailyExportJob.perform_now"
@@ -8,11 +8,8 @@ end
 every 5.minutes do
   runner "UpdateSensorsJob.perform_now"
   runner "CheckSetpointsJob.perform_now"
-  runner "CheckPowerSupplyJob.perform_now"
 end
 
 every 1.minute do
   command "/usr/bin/python /home/pi/gtv-api/log.py"
-  command "/home/pi/gtv-api/create_webserver_tunnel.sh 2>&1"
-  command "/home/pi/gtv-api/create_ssh_tunnel.sh 2>&1"
 end
