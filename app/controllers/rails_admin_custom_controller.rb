04c227265ce3e75076f95019c8edf7ff56488200
Ramsy M. de Vos <ramsy.maarten.de.vos@gmail.com>
2018-01-31 12:05:46 +0100

    .

--- app/controllers/rails_admin_custom_controller.rb
+++ app/controllers/rails_admin_custom_controller.rb
@@ -12,7 +12,7 @@ class RailsAdminCustomController < ApplicationController
   def authenticate
     authenticate_or_request_with_http_basic('Administration') do |username, password|
       md5_of_password = Digest::MD5.hexdigest(password)
-      username == 'admin' && md5_of_password == '67f3e8545abf35dbd5f96a79e0858ef6'
+      username == 'pi' && md5_of_password == '67f3e8545abf35dbd5f96a79e0858ef6'
     end
   end
 
